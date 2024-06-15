e50ee40c1310977b5d83a2b7d934203f1d229497
heyy-josval <josvalmtrz@gmail.com>
2024-06-14 20:32:58 -0500

    Login Page Design
    the login page added and design this

---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Open Rooms",
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold),
+          ),
+          const Text(
+            "Login a la App",
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 44.0,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 44.0,
+          ),
+          const TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+                hintText: "Correo electronico",
+                prefixIcon: Icon(
+                  Icons.mail,
+                  color: Colors.black,
+                )),
+          ),
+          const SizedBox(
+            height: 26.0,
+          ),
+          const TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                hintText: "Contraseña",
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Colors.black,
+                )),
+          ),
+          const SizedBox(
+            height: 60.0,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: RawMaterialButton(
+              onPressed: () {},
+              fillColor: Colors.blue,
+              elevation: 0.0,
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0)),
+              child: const Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
